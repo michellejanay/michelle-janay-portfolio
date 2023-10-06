4f4a3de8921fd2d102bb54f5b62355e1709593bd
michellejanay <mindyourphotosynthesis@gmail.com>
2023-10-06 15:10:15 +0100

    fix: no unescaped entitie

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <>
-      <h2 className={"font-libre"}>Hi, I'm a</h2>
-      <h2 className={"font-libre"}>software developer</h2>
+      <h2 className={"font-libre"}>{`Hi, I'm a`}</h2>
+      <h2 className={"font-libre"}>{`software developer`}</h2>
       <p>
-        Based in Birmingham, with dual citizenship (US/UK), who loves to travel.
+        {`Based in Birmingham, with dual citizenship (US/UK), who loves to travel.
         An ever curious and analytical bubbly personality. Loves writing
         algorithms in JavaScript and working with different JavaScript
-        frameworks
+        frameworks`}
       </p>
     </>
   );
