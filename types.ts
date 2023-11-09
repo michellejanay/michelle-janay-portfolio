a771c9144f8f9547c722ad130cb1148adf0e24a6
michellejanay <mindyourphotosynthesis@gmail.com>
2023-11-09 17:21:52 +0000

    feat: ux projects preview added

--- types.ts
+++ types.ts
@@ -3,8 +3,10 @@ export interface ProjectTypes {
   title: string;
   deployment: string;
   source_code: string;
+  figma_link: string;
   technologies: string;
   image: string;
   gif: string;
+  type: String[];
 }
 [];
